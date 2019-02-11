162bfd9bc8ad9adab20951b3de65fdabf5e0fea9
achamseddine <achamseddine@unicef.org>
2019-02-11 12:59:06 +0200

    fix R script

--- internos/activityinfo/RScripts/ai_generate_excel.R
+++ internos/activityinfo/RScripts/ai_generate_excel.R
@@ -20,9 +20,7 @@ database.id <- as.numeric(ai_id)
 if (is.na(database.id)) {
   stop("you forgot to set the database identifier at the top of this script!")
 }
-#values <- getDatabaseValueTable(database.id)
-values <- getDatabaseValueTable(database.id, col.names = c("Funded by" = "Funded_by"))
+values <- getDatabaseValueTable(database.id, col.names = c("Funded by" = "Funded_by", "Funded By" = "Funded_by"))
 values$start_date <-strftime(values$start_date,"%Y-%m-%d")
-#values <- values[values$Funded_by=="UNICEF",]
 outfilname<- paste('internos/activityinfo/AIReports/', ai_id, "_ai_data.csv", sep="")
 write.csv(values, outfilname, row.names=FALSE)
