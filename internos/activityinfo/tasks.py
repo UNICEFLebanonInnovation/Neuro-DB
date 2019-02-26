941dbb6a6d59cca8f244e12ad055f06c029ebff6
achamseddine <achamseddine@unicef.org>
2019-02-26 19:30:59 +0200

    Split auto script

--- internos/activityinfo/tasks.py
+++ internos/activityinfo/tasks.py
@@ -29,19 +29,27 @@ def read_imported_data():
 
 
 @app.task
-def calculate_live_values():
-    from internos.activityinfo.utils import sync_live_data, link_indicators_data, reset_indicators_values, calculate_indicators_values
+def import_live_data():
     from internos.activityinfo.models import Database
 
     databases = Database.objects.filter(reporting_year__current=True)
     for db in databases:
         print('1. Import report')
         r_script_command_line('ai_generate_excel.R', db)
-        print('2. Import data forced')
+
+
+@app.task
+def calculate_live_values():
+    from internos.activityinfo.utils import sync_live_data, link_indicators_data, reset_indicators_values, calculate_indicators_values
+    from internos.activityinfo.models import Database
+
+    databases = Database.objects.filter(reporting_year__current=True)
+    for db in databases:
+        print('1. Import data forced')
         sync_live_data(db)
-        print('3. Link indicators')
+        print('2. Link indicators')
         link_indicators_data(db, report_type='live')
-        print('4. Reset values')
+        print('3. Reset values')
         reset_indicators_values(db.ai_id, report_type='live')
-        # print('1. Calculate indicator values')
+        # print('4. Calculate indicator values')
         # calculate_indicators_values(db, report_type='live')
