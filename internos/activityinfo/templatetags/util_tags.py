739d6651732dc3c5ec06cc1e2cce68825928b5fa
achamseddine <achamseddine@unicef.org>
2019-08-16 12:31:54 +0300

    Remove printing

--- internos/activityinfo/templatetags/util_tags.py
+++ internos/activityinfo/templatetags/util_tags.py
@@ -446,7 +446,6 @@ def get_indicator_hpm_data(ai_id, month=None):
 
         values_hpm = indicator.values_hpm
         previous_month = int(month) - 1
-        print(previous_month)
         last_month_value = 0
 
         if str(previous_month) in indicator.cumulative_values_hpm['months']:
@@ -472,13 +471,10 @@ def get_indicator_hpm_data(ai_id, month=None):
         tag_prog_cbece = float(indicator.values_tags['CBECE']) if 'CBECE' in indicator.values_tags else 0.0
         tag_prog_alp = float(indicator.values_tags['ALP']) if 'ALP' in indicator.values_tags else 0.0
 
-        print(indicator.cumulative_values_hpm)
-
         if 'tags' in indicator.cumulative_values_hpm:
             last_month_value_tag = indicator.cumulative_values_hpm['tags']
             key1 = '{}-{}'.format(month, 'BLN')
             key1a = '{}-{}'.format(previous_month, 'BLN')
-            print(key1a)
             key2 = '{}-{}'.format(month, 'CBECE')
             key2a = '{}-{}'.format(previous_month, 'CBECE')
             key3 = '{}-{}'.format(month, 'ALP')
@@ -486,7 +482,6 @@ def get_indicator_hpm_data(ai_id, month=None):
             if key1a in last_month_value_tag:
                 # tag_prog_bln = last_month_value_tag[key1]
                 last_report_changes_bln = tag_prog_bln - float(last_month_value_tag[key1a])
-                print(last_report_changes_bln)
             if key2a in last_month_value_tag:
                 # tag_prog_cbece = last_month_value_tag[key2]
                 last_report_changes_cbece = tag_prog_cbece - float(last_month_value_tag[key2a])
