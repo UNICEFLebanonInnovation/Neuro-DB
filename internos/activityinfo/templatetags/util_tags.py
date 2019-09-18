0ec543f10f7c5ee7279570b0ab26864f272435c8
achamseddine <achamseddine@unicef.org>
2019-09-18 10:42:21 +0300

    Remove debug 2

--- internos/activityinfo/templatetags/util_tags.py
+++ internos/activityinfo/templatetags/util_tags.py
@@ -537,12 +537,7 @@ def get_indicator_hpm_data(ai_id, month=None):
         cumulative_result = "{:,}".format(round(cumulative), 1)
         cumulative_result = cumulative_result.replace('.0', '')
 
-        if int(month) > 1 and cumulative:
-            last_month_value = cumulative
-            print(cumulative)
-            print(last_month_value)
-
-        if last_month_value == 0:
+        if int(month) == 1:
             last_report_changes = 0
         else:
             last_report_changes = "{:,}".format(round(int(cumulative) - int(last_month_value), 1))
