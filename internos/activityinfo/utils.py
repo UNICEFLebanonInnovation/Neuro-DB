4a10e832db92d352cf06676415b44534907018a8
Marwa Ayoub <60692633+maayoub@users.noreply.github.com>
2020-04-07 17:03:07 +0300

    fix month name in template and table of hpm

--- internos/activityinfo/utils.py
+++ internos/activityinfo/utils.py
@@ -2242,7 +2242,28 @@ def update_hpm_table_docx(indicators, month, month_name, filename,reporting_year
 
     document = Document(path2file)
     # month = month - 1
-    document.paragraphs[0].runs[1].text = month_name
+    # document.paragraphs[0].runs[1].text = month_name
+    replacements = {
+        'month_name': month_name ,
+    }
+    #
+    # for paragraph in document.paragraphs:
+    #     for key in replacements:
+    #         paragraph.text = paragraph.text.replace(key, replacements[key])
+    # document.paragraphs[0].runs[0].style
+    #
+    # for paragraph in document.tables[0].rows[0].cells[1].paragraphs:
+    #     for key in replacements:
+    #         paragraph.text = paragraph.text.replace(key, replacements[key])
+    if month == 1:
+        document.paragraphs[0].add_run('{} {} {} {}'.format('HPM Table | Data of ', month_name ,'|',reporting_year))
+
+        document.tables[0].rows[0].cells[1].paragraphs[0].runs[0].text='{} {} {} {} {}'.format('SUMMARY OF PROGRAMME RESULTS |', month_name,'|' ,reporting_year,' SITREP - LEBANON')
+    else:
+        document.paragraphs[0].add_run('{} {} {} {}'.format('HPM Table | Data of  Janurary to ', month_name, '|', reporting_year))
+
+        document.tables[0].rows[0].cells[1].paragraphs[0].runs[0].text = '{} {} {} {} {}'.format('SUMMARY OF PROGRAMME RESULTS | January to', month_name, '|', reporting_year, ' SITREP - LEBANON')
+
     databases = Database.objects.filter(reporting_year__name=reporting_year).order_by('hpm_sequence')
 
     edu_list=[]
